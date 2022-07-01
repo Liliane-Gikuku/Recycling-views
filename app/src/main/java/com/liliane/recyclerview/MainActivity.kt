09d56640de36ee5b606459e64bc42db8ff898b0b
Liliane-Gikuku <lilkundiro@gmail.com>
2022-07-01 20:36:46 +0300

    Recycler view

--- app/src/main/java/com/liliane/recyclerview/MainActivity.kt
+++ app/src/main/java/com/liliane/recyclerview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.liliane.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.liliane.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+        displayNames()
+    }
+
+    fun displayNames(){
+        val namesList=listOf("Feyi","Roique")
+        binding.rvNames.layoutManager=LinearLayoutManager(this)
+        val namesAdapter=NamesRecyclerViewAdapter(namesList)
+        binding.rvNames.adapter=namesAdapter
     }
 }
---
+++ app/src/main/java/com/liliane/recyclerview/NamesRecyclerViewAdapter.kt
@@ -0,0 +1,34 @@
+package com.liliane.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NamesRecyclerViewAdapter(var namesList:List<String>): RecyclerView.Adapter<NamesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NamesViewHolder {
+       var itemView=LayoutInflater.from(parent.context).inflate(R.layout.name_list_item,parent,false)
+        return NamesViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NamesViewHolder, position: Int) {
+        holder.tvName.text=namesList.get(position)
+    }
+
+    override fun getItemCount(): Int {
+        return namesList.size
+    }
+}
+
+class NamesViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+    var tvName=itemView.findViewById<TextView>(R.id.tvName)
+}
+
+
+
+
+
+
+//principles of object oriented programming, abstraction
+//a room you were hosted in at first will remain but you hardly realize it still exists
